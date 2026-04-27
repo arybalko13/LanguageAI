c786eec868a2e00e6de2ec21ad7bbdd09f37f0a5
arybalko13 <arybalko@students.uni-mainz.de>
2026-04-27 22:53:40 +0200

    Update 01_average_length.py

--- LanguageAI/01_Einfu╠êhrung/02_Homework/level_3/01_average_length.py
+++ LanguageAI/01_Einfu╠êhrung/02_Homework/level_3/01_average_length.py
@@ -22,8 +22,14 @@ on Python 3."""]
 # Ermitteln Sie die durchschnittliche Zeichenlänge und Wortlänge der drei Strings!
 # Schreiben Sie den Code so, dass er auch dann noch funktioniert, wenn der Liste ein Element hinzugefügt wird!
 
-average_segment_chars = 0
-average_segment_words = 0
+sample_string_one = "Python is an interpreted, high-level and general-purpose programming language.  Python's design philosophy emphasizes code readability with its notable use of significant indentation. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
+sample_string_two = "Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly, procedural), object-oriented and functional programming. Python is often described as a batteries included language due to its comprehensive standard library."
+sample_string_three = "Guido van Rossum began working on Python in the late 1980's, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.1. Python 2.0 was released in 2000 and introduced new features, such as list comprehensions and a garbage collection system using reference counting and was discontinued with version 2.7.18 in 2020. Python 3.0 was released in 2008 and was a major revision of the language that is not completely backward-compatible and much Python 2 code does not run unmodified on Python 3."
+
+average_segment_chars = (len(sample_string_one)+len(sample_string_two)+len(sample_string_three))/3
+
+
+average_segment_words = (len(sample_string_one.split())+len(sample_string_two.split())+len(sample_string_three.split()))/3
 
 print(average_segment_chars)
 print(average_segment_words)
